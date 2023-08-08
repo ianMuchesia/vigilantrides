9b162c56dc73f004d2eb539e74cb989c107eb9c3
ianMuchesia <ianmuchesia@yahoo.com>
2023-08-08 09:46:54 +0300

    first commit

---
+++ views/login.php
@@ -0,0 +1,15 @@
+<div class="flex flex-col items-center justify-center xl:flex-row gap-5 w-screen  h-screen">
+        <div class="p-8 flex justify-center items-center xl:w-[50%]">
+        <img src="img/electric-bus.png" alt="" class="max-w-full h-96 object-contain ">
+    </div>
+        <form action="../index.php" method="post" class="flex flex-col p-8 gap-16">
+            <input type="text" name="" id=""  class="bg-gray-100 p-2 border-4 border-white border-b-gray-500"placeholder="username">
+            <input type="password" name="" id=""
+            placeholder="password"
+            class="bg-gray-100 p-2 border-4 border-white border-b-gray-500" >
+            <button class="bg-gray-100 p-2 border-4" name="login">
+                Login
+            </button>
+        </form>
+
+        </div>
